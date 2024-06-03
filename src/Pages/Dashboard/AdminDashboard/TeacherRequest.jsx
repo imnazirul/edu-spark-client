82d74ecf44d9f1270de08db98ac3500ea75db132
nazirul <nazirul.me01c@gmail.com>
2024-06-03 10:59:02 +0600

    added teacher request page for admin

--- src/Pages/Dashboard/AdminDashboard/TeacherRequest.jsx
+++ src/Pages/Dashboard/AdminDashboard/TeacherRequest.jsx
@@ -1,5 +1,103 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../CustomHooks/useAxiosSecure";
+
 const TeacherRequest = () => {
-  return <div className="">teacher request</div>;
+  const axiosSecure = useAxiosSecure();
+
+  const {
+    data: teacherRequests = [],
+    isPending,
+    isError,
+  } = useQuery({
+    queryKey: ["teachersRequest"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("teacher_request");
+      return res.data;
+    },
+  });
+
+  const handleReqApprove = (id) => {
+    console.log("approve", id);
+  };
+  const handleReqReject = (id) => {
+    console.log("reject", id);
+  };
+
+  if (isPending) {
+    <h1 className="text-5xl text-center mt-10">Loading...</h1>;
+  }
+
+  return (
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table text-center">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>IMAGE </th>
+              <th>NAME</th>
+              <th>EXPERIENCE</th>
+              <th>TITLE</th>
+              <th>CATEGORY</th>
+              <th>STATUS</th>
+              <th>ACTION</th>
+            </tr>
+          </thead>
+          <tbody>
+            {teacherRequests.map((request) => (
+              <tr key={request._id}>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src={request.image}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td>{request?.name}</td>
+                <td>{request?.experience}</td>
+                <td>{request?.title}</td>
+                <td>{request?.category}</td>
+                <td>
+                  <span
+                    className={`${
+                      request.status === "approved"
+                        ? "text-green-500 bg-green-500"
+                        : request.status === "rejected"
+                        ? "text-green-500 bg-green-500"
+                        : "text-orange-500 bg-orange-500"
+                    } bg-opacity-15 rounded-3xl px-3 py-1`}
+                  >
+                    {request?.status}
+                  </span>
+                </td>
+                <td>
+                  <div className="flex gap-1 items-center">
+                    <button
+                      onClick={handleReqApprove}
+                      className="btn mr-1 bg-green-500 text-white btn-xs"
+                    >
+                      Approve
+                    </button>
+                    <button
+                      onClick={handleReqReject}
+                      className="btn bg-red-500 text-white btn-xs"
+                    >
+                      Reject
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default TeacherRequest;
--- src/Pages/TeachOnEdu/TeachOnEdu.jsx
+++ src/Pages/TeachOnEdu/TeachOnEdu.jsx
@@ -1,8 +1,51 @@
 /* eslint-disable no-unused-vars */
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../CustomHooks/useAxiosSecure";
+import useAuth from "../../CustomHooks/useAuth";
+import Swal from "sweetalert2";
+import { useState } from "react";
+import AutorenewRoundedIcon from "@mui/icons-material/AutorenewRounded";
+import QueryBuilderIcon from "@mui/icons-material/QueryBuilder";
 
 const TeachOnEdu = () => {
+  const axiosSecure = useAxiosSecure();
+  const { user } = useAuth();
+  const [appliedExists, setAppliedExists] = useState(null);
+
+  const {
+    mutate: applyForTeaching,
+    isPending: isPostPending,
+    isSuccess,
+    isError,
+  } = useMutation({
+    mutationFn: async (data) => {
+      const res = await axiosSecure.post("/teacher_requests", data);
+      return res.data;
+    },
+    onSuccess: (res) => {
+      if (res.insertedId) {
+        Swal.fire({
+          title: "Submitted Successfully!",
+          text: "Please Wait You Got Any Response",
+          icon: "success",
+        });
+      }
+    },
+  });
+
+  // check if his any application already in Pending
+  const { data: applications = [], isPending } = useQuery({
+    queryKey: ["applications"],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/teacher_requests/${user?.email}`);
+      setAppliedExists(res.data.find((sData) => sData.status === "pending"));
+      console.log(res.data);
+      return res.data;
+    },
+  });
+
   const {
     register,
     handleSubmit,
@@ -10,8 +53,19 @@ const TeachOnEdu = () => {
   } = useForm();
 
   const handleApply = (data) => {
-    console.log(data);
+    const teacherInfo = {
+      ...data,
+      name: user?.displayName,
+      image: user?.photoURL,
+      email: user?.email,
+      status: "pending",
+    };
+    applyForTeaching(teacherInfo);
   };
+
+  if (isPending) {
+    return <h1 className="text-5xl text-center mt-10">Loading...</h1>;
+  }
 
   return (
     <div>
@@ -20,90 +74,161 @@ const TeachOnEdu = () => {
         subtitle="Become an instructor and change lives — including your own"
       ></SectionTitle>
       <div className="mt-8">
-        <div className="divider mb-8 divider-primary">
-          {" "}
-          <h1 className="text-3xl text-primary-1 text-center font-semibold">
-            — APPLY NOW —
-          </h1>
-        </div>
+        {!appliedExists && (
+          <div className="divider mb-8 divider-primary">
+            {" "}
+            <h1 className="text-3xl text-primary-1 text-center font-semibold">
+              — APPLY NOW —
+            </h1>
+          </div>
+        )}
 
         <div className="flex justify-evenly">
           <div>
             <div className="flex-1 ">
-              <form onSubmit={handleSubmit(handleApply)} className="flex-1">
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text text-white text-lg">
-                      Select Your Experience Level
-                    </span>
-                  </label>
-                  <select
-                    defaultValue="default"
-                    {...register("experience", {
-                      required: {
-                        value: true,
-                        message: "Experience is a Required Field!",
-                      },
-                    })}
-                    className="select select-bordered w-full max-w-xs"
-                  >
-                    <option disabled value="default">
-                      Select Experience Level
-                    </option>
-                    <option value="Beginner">Beginner</option>
-                    <option value="Mid-Level">Mid-Level</option>
-                    <option value="Experienced">Experienced</option>
-                  </select>
-                </div>
-
-                <div className="form-control relative">
-                  <label className="label">
-                    <span className="label-text text-white text-lg">Title</span>
-                  </label>
-                  <input
-                    {...register("title", {
-                      required: {
-                        value: true,
-                        message: "Title is a Required Field!",
-                      },
-                    })}
-                    placeholder="Title"
-                    className="input w-full input-bordered"
-                  />
-                </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text text-white text-lg">
-                      Select Category
-                    </span>
-                  </label>
-                  <select
-                    defaultValue="default"
-                    {...register("category", {
-                      required: {
-                        value: true,
-                        message: "Category is a Required Field!",
-                      },
-                    })}
-                    className="select select-bordered w-full max-w-xs"
-                  >
-                    <option disabled value="default">
-                      Select Category
-                    </option>
-                    <option>Web Development</option>
-                    <option>App Development</option>
-                    <option>Cyber Security</option>
-                    <option>Web Design</option>
-                    <option>Graphic Design</option>
-                    <option>Digital Marketing</option>
-                  </select>
-                </div>
-                <div className="form-control mt-6">
+              {appliedExists ? (
+                <>
                   <button className="btn bg-primary-1 hover:bg-primary-1 text-lg text-white hover:bg-btn-1">
-                    SUBMIT FOR REVIEW
+                    <QueryBuilderIcon></QueryBuilderIcon> YOUR REQUEST IS IN
+                    PENDING...
                   </button>
-                </div>
-              </form>
+
+                  <p className="mt-5 text-xl font-semibold ">
+                    YOUR SUBMITTED INFO:
+                  </p>
+                  <form className="flex-1">
+                    <div className="form-control">
+                      <label className="label">
+                        <span className="label-text  text-lg">
+                          Experience Level
+                        </span>
+                      </label>
+                      <input
+                        readOnly
+                        defaultValue={appliedExists.experience}
+                        placeholder="Title"
+                        className="input w-full input-bordered"
+                      />
+                    </div>
+
+                    <div className="form-control relative">
+                      <label className="label">
+                        <span className="label-text  text-lg">Title</span>
+                      </label>
+                      <input
+                        readOnly
+                        defaultValue={appliedExists.title}
+                        placeholder="Title"
+                        className="input w-full input-bordered"
+                      />
+                    </div>
+                    <div className="form-control">
+                      <label className="label">
+                        <span className="label-text  text-lg">Category</span>
+                      </label>
+                      <input
+                        readOnly
+                        defaultValue={appliedExists.category}
+                        placeholder="Title"
+                        className="input w-full input-bordered"
+                      />
+                    </div>
+                    <button className="btn bg-primary-1 hover:bg-primary-1 mt-3 text-lg text-white hover:bg-btn-1">
+                      <AutorenewRoundedIcon></AutorenewRoundedIcon>WE WILL
+                      RESPONSE SHORTLY...
+                    </button>
+                  </form>
+                </>
+              ) : (
+                <form onSubmit={handleSubmit(handleApply)} className="flex-1">
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text  text-lg">
+                        Select Your Experience Level
+                      </span>
+                    </label>
+                    <select
+                      defaultValue="default"
+                      {...register("experience", {
+                        required: {
+                          value: true,
+                          message: "Experience is a Required Field!",
+                        },
+                      })}
+                      className="select select-bordered w-full max-w-xs"
+                    >
+                      <option disabled value="default">
+                        Select Experience Level
+                      </option>
+                      <option value="Beginner">Beginner</option>
+                      <option value="Mid-Level">Mid-Level</option>
+                      <option value="Experienced">Experienced</option>
+                    </select>
+                  </div>
+
+                  <div className="form-control relative">
+                    <label className="label">
+                      <span className="label-text  text-lg">Title</span>
+                    </label>
+                    <input
+                      {...register("title", {
+                        required: {
+                          value: true,
+                          message: "Title is a Required Field!",
+                        },
+                      })}
+                      placeholder="Title"
+                      className="input w-full input-bordered"
+                    />
+                  </div>
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text  text-lg">
+                        Select Category
+                      </span>
+                    </label>
+                    <select
+                      defaultValue="default"
+                      {...register("category", {
+                        required: {
+                          value: true,
+                          message: "Category is a Required Field!",
+                        },
+                      })}
+                      className="select select-bordered w-full max-w-xs"
+                    >
+                      <option disabled value="default">
+                        Select Category
+                      </option>
+                      <option>Web Development</option>
+                      <option>App Development</option>
+                      <option>Cyber Security</option>
+                      <option>Web Design</option>
+                      <option>Graphic Design</option>
+                      <option>Digital Marketing</option>
+                    </select>
+                  </div>
+                  <div className="form-control mt-6">
+                    {applications.length > 0 ? (
+                      <button className="btn bg-primary-1 hover:bg-primary-1 text-lg text-white hover:bg-btn-1">
+                        {isPostPending
+                          ? "SUBMITTING..."
+                          : isSuccess
+                          ? "SUBMITTED"
+                          : " REQUEST FOR ANOTHER"}
+                      </button>
+                    ) : (
+                      <button className="btn bg-primary-1 hover:bg-primary-1 text-lg text-white hover:bg-btn-1">
+                        {isPostPending
+                          ? "SUBMITTING..."
+                          : isSuccess
+                          ? "SUBMITTED"
+                          : "SUBMIT FOR REVIEW"}
+                      </button>
+                    )}
+                  </div>
+                </form>
+              )}
             </div>
           </div>
 
@@ -112,7 +237,7 @@ const TeachOnEdu = () => {
           <div className=" flex flex-col justify-center items-center">
             <div className="avatar">
               <div className="w-16 rounded-full">
-                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                <img src={user?.photoURL} />
               </div>
             </div>
             <div className="text-center mb-5">
